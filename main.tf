957cb16a6def4ead0ed3fa4bcfc0e49657bc926e
mitraman <ronnie.mitra@gmail.com>
2020-06-19 16:57:01 +0100

    changed name of vpc

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
   cidr_block = var.main_vpc_cidr
 
   tags = {
-    "Name"                                        = "${var.env_name} ${var.vpc_name}",
+    "Name"                                        = "${var.vpc_name}-${var.env_name} ",
     "kubernetes.io/cluster/${local.cluster_name}" = "shared",
   }
 }
